29e54ed2357553be0b35f8a14bee0a1f77a73f24
Aschoolcoder <89813262+Aschoolcoder@users.noreply.github.com>
2021-10-11 11:01:24 -0700

    update

--- Testing/Graphic.py
+++ Testing/Graphic.py
@@ -9,14 +9,15 @@ arcade.start_render()
 # Color variables
 GOLD = 199, 164, 70
 MASTER_SWORD_PURPLE = 20, 17, 99
+BLADE_GRAY = 216, 223, 235
 
 
 # Flower function
 def draw_flower(x, y):
     """draws a flower
     idk how to get tilt angle"""
-    arcade.draw_rectangle_filled(450 + x, 350 + y, 10, 75, (168, 171, 82))
-    arcade.draw_circle_filled(450 + x, 380 + y, 15, (230, 225, 195))
+    arcade.draw_rectangle_filled(x, y, 10, 75, (168, 171, 82))
+    arcade.draw_circle_filled(x, y + 40, 15, (230, 225, 195))
 
 
 # pedestal
@@ -26,15 +27,15 @@ arcade.draw_rectangle_outline(500, 300, 200, 120, (93, 99, 24), 5)
 arcade.draw_rectangle_filled(500, 300, 50, 30, (76, 87, 68))
 
 # Blade
-arcade.draw_rectangle_filled(500, 400, 35, 200, (216, 223, 235))
-arcade.draw_triangle_filled(500, 480, 525, 500, 475, 500, (216, 223, 235))
-arcade.draw_rectangle_filled(500, 517.5, 49, 35, (216, 223, 235))
-arcade.draw_triangle_filled(500, 555, 525, 535, 475, 535, (216, 223, 235))
-arcade.draw_rectangle_filled(500, 575, 30, 100, (216, 223, 235))
+arcade.draw_rectangle_filled(500, 400, 35, 200, BLADE_GRAY)
+arcade.draw_triangle_filled(500, 480, 525, 500, 475, 500, BLADE_GRAY)
+arcade.draw_rectangle_filled(500, 517.5, 49, 35, BLADE_GRAY)
+arcade.draw_triangle_filled(500, 555, 525, 535, 475, 535, BLADE_GRAY)
+arcade.draw_rectangle_filled(500, 575, 30, 100, BLADE_GRAY)
 
 # Blade-hilt intersection
-arcade.draw_rectangle_filled(500, 600, 30, 60, (20, 17, 99))
-arcade.draw_ellipse_filled(500, 570, 22, 50, (216, 223, 235))
+arcade.draw_rectangle_filled(500, 600, 30, 60, MASTER_SWORD_PURPLE)
+arcade.draw_ellipse_filled(500, 570, 22, 50, BLADE_GRAY)
 arcade.draw_rectangle_filled(487, 570, 5, 5, GOLD)
 arcade.draw_rectangle_filled(513, 570, 5, 5, GOLD)
 arcade.draw_triangle_filled(500, 600, 505, 610, 495, 610, GOLD)
@@ -49,7 +50,7 @@ arcade.draw_polygon_filled(((485, 620),
                             (465, 610),
                             (485, 600),
                             ),
-                           (20, 17, 99))
+                           MASTER_SWORD_PURPLE)
 arcade.draw_polygon_filled(((515, 620),
                             (540, 630),
                             (545, 635),
@@ -57,7 +58,7 @@ arcade.draw_polygon_filled(((515, 620),
                             (535, 610),
                             (515, 600),
                             ),
-                           (20, 17, 99))
+                           MASTER_SWORD_PURPLE)
 arcade.draw_arc_filled(470, 600, 60, 60, MASTER_SWORD_PURPLE, 20, 180, 90)
 arcade.draw_rectangle_filled(460, 570, 30, 30, arcade.csscolor.GREY, 75)
 arcade.draw_arc_filled(530, 600, 60, 60, MASTER_SWORD_PURPLE, 0, 163, 270)
@@ -88,7 +89,7 @@ arcade.draw_rectangle_filled(490, 519.5, 7, 3, (201, 201, 201))
 # Flowers
 arcade.draw_rectangle_filled(450, 350, 10, 75, (168, 171, 82), 170)
 arcade.draw_circle_filled(445, 380, 15, (230, 225, 195))
-draw_flower(100, -12)
+draw_flower(550, 335)
 
 arcade.finish_render()
 
---
+++ Testing/Scratch.py
@@ -0,0 +1,102 @@
+import arcade
+
+arcade.open_window(1000, 800, 'master sword')
+
+arcade.set_background_color(arcade.csscolor.GREY)
+
+# Color variables
+GOLD = 199, 164, 70
+MASTER_SWORD_PURPLE = 20, 17, 99
+
+
+def master_sword(x, y):
+    # Blade
+    arcade.draw_rectangle_filled(x, y, 35, 200, (216, 223, 235))
+    arcade.draw_triangle_filled(x, y + 80, x + 25, y + 100, x - 25, y + 100, (216, 223, 235))
+    arcade.draw_rectangle_filled(x, y + 117.5, 49, 35, (216, 223, 235))
+    arcade.draw_triangle_filled(x, y + 155, x + 25, y + 135, x - 25, y + 135, (216, 223, 235))
+    arcade.draw_rectangle_filled(x, y + 175, 30, 100, (216, 223, 235))
+
+    # Blade-hilt intersection
+    arcade.draw_rectangle_filled(x, y + 200, 30, 60, (20, 17, 99))
+    arcade.draw_ellipse_filled(x, y + 170, 22, 50, (216, 223, 235))
+    arcade.draw_rectangle_filled(x - 13, y + 170, 5, 5, GOLD)
+    arcade.draw_rectangle_filled(x + 13, y + 170, 5, 5, GOLD)
+    arcade.draw_triangle_filled(x, y + 200, x + 5, y + 210, x - 5, y + 210, GOLD)
+    arcade.draw_triangle_filled(x, y + 220, x + 5, y + 210, x - 5, y + 210, GOLD)
+    arcade.draw_rectangle_filled(x, y + 199, 4, 10, GOLD)
+
+    # Wings
+    arcade.draw_polygon_filled(((x - 15, y + 220),
+                                (x - 40, y + 230),
+                                (x - 45, y + 235),
+                                (x - 40, y + 225),
+                                (x - 35, y + 210),
+                                (x - 15, y + 200),
+                                ),
+                               (20, 17, 99))
+    arcade.draw_polygon_filled(((x + 15, y + 220),
+                                (x + 40, y + 230),
+                                (x + 45, y + 235),
+                                (x + 40, y + 225),
+                                (x + 35, y + 210),
+                                (x + 15, y + 200),
+                                ),
+                               (20, 17, 99))
+    arcade.draw_arc_filled(x - 30, y + 200, 60, 60, MASTER_SWORD_PURPLE, 20, 180, 90)
+    arcade.draw_rectangle_filled(x - 40, y + 170, 30, 30, arcade.csscolor.GREY, 75)
+    arcade.draw_arc_filled(x + 30, y + 200, 60, 60, MASTER_SWORD_PURPLE, 0, 163, 270)
+    arcade.draw_rectangle_filled(x + 40, y + 170, 30, 30, arcade.csscolor.GREY, 285)
+
+    # beginning of handle
+    arcade.draw_arc_filled(x - 15, y + 220, 13, 8, arcade.csscolor.GREY, 0, 150)
+    arcade.draw_arc_filled(x + 15, y + 220, 13, 8, arcade.csscolor.GREY, 30, 180)
+
+    # handle
+    arcade.draw_rectangle_filled(x, y + 260, 13, 65, MASTER_SWORD_PURPLE)
+    arcade.draw_polygon_filled(((x - 3, y + 280),
+                                (x - 10, y + 290),
+                                (x - 5, y + 300),
+                                (x + 5, y + 300),
+                                (x + 10, y + 290),
+                                (x + 3, y + 280),
+                                ),
+                               MASTER_SWORD_PURPLE)
+
+    # Tri-force inscription
+    arcade.draw_triangle_outline(x, y + 107.5, x + 10, y + 125.5, x - 10, y + 125.5, (201, 201, 201), 3)
+    arcade.draw_triangle_outline(x, y + 125.5, x + 4, y + 117.5, x - 4, y + 117.5, (201, 201, 201), 3)
+    arcade.draw_rectangle_filled(x, y + 127.5, 3, 10, (201, 201, 201))
+    arcade.draw_rectangle_filled(x + 10, y + 119.5, 7, 3, (201, 201, 201))
+    arcade.draw_rectangle_filled(x - 10, y + 119.5, 7, 3, (201, 201, 201))
+
+
+def normal_screen(delta_time):
+    arcade.start_render()
+
+    master_sword(500, 400)
+
+
+def on_draw(delta_time):
+    arcade.draw_rectangle_filled(500, 400, 100000, 100000000, arcade.csscolor.GREY)
+
+    master_sword(500, 430)
+
+
+def phase_2(delta_time):
+    arcade.draw_rectangle_filled(500, 400, 100000, 100000000, arcade.csscolor.GREY)
+
+    master_sword(500, 450)
+
+    arcade.finish_render()
+
+
+def main():
+    arcade.schedule(normal_screen, 0 / 1)
+    arcade.schedule(on_draw, 2 / 1)
+    arcade.schedule(phase_2, 3 / 1)
+
+
+main()
+
+arcade.run()

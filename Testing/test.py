975ef994b5d13acf62b6690586aedf46b622b003
Aschoolcoder <89813262+Aschoolcoder@users.noreply.github.com>
2021-12-01 10:20:58 -0800

    update

--- Testing/test.py
+++ Testing/test.py
@@ -1,2 +1,36 @@
-for i in range(5, 301, 10):
-    print(i % 4)
+# for i in range(10):
+#     print("*", end=" ")
+
+# for e in range(10):
+#   print("*", end=" ")
+# print("  ")
+# for e in range(5):
+#    print("*", end=" ")
+# print("  ")
+# for e in range(20):
+#     print("*", end=" ")
+
+# for row in range(10):
+#     print(" ")
+#     for column in range(10):
+#         print(end="  *")
+
+# for row in range(10):
+#     print(" ")
+#     for column in range(5):
+#         print(end="  *")
+
+# for row in range(5):
+#     print(" ")
+#     for column in range(20):
+#         print(end="  *")
+
+#for row in range(10):
+#    print(" ")
+#    for column in range(10):
+#       print(column, end="  ")
+
+for row in range(10):
+    print(" ")
+    for column in range(10):
+        print(row, end="  ")
